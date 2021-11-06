fd7aa2f5e5075edcde86ee51e033e809f34296cb
dinoarceuxis1@gmail.com <dinoarceuxis1@gmail.com>
2021-11-06 18:20:52 +0100

    Implementacion inicial de Login y la Splash Screen

---
+++ app/src/main/java/com/example/truetaxi/ActivitySplashScreen.java
@@ -0,0 +1,17 @@
+package com.example.truetaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
+
+public class ActivitySplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
