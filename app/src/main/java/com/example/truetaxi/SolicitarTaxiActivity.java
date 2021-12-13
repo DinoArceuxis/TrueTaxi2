f3ac4461a4c71b424d368bb4bffd1a39847e3c63
dinoarceuxis1@gmail.com <dinoarceuxis1@gmail.com>
2021-12-13 15:33:06 +0100

    (R5) Añadida la seccion de Solicitud de Taxi. Version inicial, falta decidir si con mapa o seleccionando directamente.

--- app/src/main/java/com/example/truetaxi/SolicitarTaxiActivity.java
+++ app/src/main/java/com/example/truetaxi/SolicitarTaxiActivity.java
@@ -2,13 +2,36 @@ package com.example.truetaxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SolicitarTaxiActivity extends AppCompatActivity {
 
+    private Spinner sp1;
+    private EditText etorigen,etfecha,ethora;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solicitar_taxi);
+
+        etorigen=findViewById(R.id.origen);
+        etfecha=findViewById(R.id.fecha);
+        ethora=findViewById(R.id.hora);
+        etorigen.setText("ETSINF - Boadilla del Monte");
+        sp1=findViewById(R.id.destinospinner);
+        String [] opciones={"ETSINF - Boadilla del Monte","Calle Puerta del Angel 31","Principe Pio 23","Calle Arrakeen 4","Calle Gran Via 12","Intercambiador de Moncloa"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, opciones);
+        sp1.setAdapter(adapter);
+    }
+
+    public void solicitar_taxi(View view)
+    {
+        Toast.makeText(SolicitarTaxiActivity.this, "Solicitud de Taxi Enviada.", Toast.LENGTH_LONG).show();
+        finish();
     }
 }
